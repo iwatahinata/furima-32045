0985b85988d0aac4ee247f5d7c7e71bc448a4238
iwatahinata <iwata.10120612@gmail.com>
2020-11-18 17:01:11 +0900

    user.rbのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
+  validates :nickname, presence: true
   
 end
