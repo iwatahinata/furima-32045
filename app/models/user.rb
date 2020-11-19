6972af25ed4d1d87bdd31d571b78f0581b329a07
iwatahinata <iwata.10120612@gmail.com>
2020-11-19 11:00:47 +0900

    user.rbのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,22 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+ with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
   validates :first_name, presence: true
-  validates :first_name_kana, presence: true
   validates :last_name, presence: true
+ end
+ with_options format: { with: /\A[ァ-ケ一ー]+\z/, message: '全角カタカナを使用してください' } do
+  validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
+ end
   validates :birthday, presence: true
   validates :nickname, presence: true
-  
+  validates :email, uniqueness: true,
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
+  validates :password_confirmation, presence: true
 end
