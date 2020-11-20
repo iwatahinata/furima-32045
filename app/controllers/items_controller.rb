ccdb26128747c60d27389943abd8d124bdf456d9
iwatahinata <iwata.10120612@gmail.com>
2020-11-20 11:22:25 +0900

    商品出品機能のルーティング作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :image, :explain, :category_id, :status_id, :derivery_fee_id, :derivery_area_id, :derivery_days_id, :price)
+  end
 end
