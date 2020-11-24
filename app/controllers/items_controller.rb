a5da8b5e80d0fac9d4ac4e53aacf281d12f24e6b
iwatahinata <iwata.10120612@gmail.com>
2020-11-24 15:47:10 +0900

    updateに関するコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new]
+  before_action :set_item, only: :edit
+  before_action :move_to_index, only: :edit
   
   def index
     @items = Item.all.order("id DESC")
@@ -40,4 +42,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :image, :explain, :category_id, :status_id, :fee_id, :days_id, :price, :prefecture_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+  
 end
