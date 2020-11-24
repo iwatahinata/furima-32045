9e0b0461ffaf9fee652a24a36f41d78931db8fa6
iwatahinata <iwata.10120612@gmail.com>
2020-11-24 16:40:23 +0900

    商品削除機能におけるコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new, :edit, :update, :delete]
-  before_action :set_item, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :update, :destroy]
   before_action :move_to_index, only: :edit
   
   def index
@@ -36,9 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if corrent_user.id = @item.user_id
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
 
